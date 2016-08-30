7296acb9d52821e98419a12929b91c1866155a7b
Marco Lopez <mlopez@marostdevelopers.com>
2016-08-29 22:10:45 -0500

    agregando archivo helpers

---
+++ app/helpers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Jenssegers\Date\Date;
+Date::setLocale('es');
+
+/* FECHA ACTUAL */
+function fechaActual()
+{
+    $fechaActual = date('Y-m-d H:i:s');
+    return $fechaActual;
+}
+
+function fechaTexto($datetime)
+{
+    $fecha = Date::create($datetime->year, $datetime->month, $datetime->day, $datetime->hour, $datetime->minute, $datetime->second);
+    $fecha = $fecha->format('d \\d\\e F \\d\\e\\l Y');
+    return $fecha;
+}
+
+function fechaDia($datetime)
+{
+    $fecha = Date::create($datetime->year, $datetime->month, $datetime->day, $datetime->hour, $datetime->minute, $datetime->second);
+    $fecha = $fecha->format('d');
+    return $fecha;
+}
+
+function fechaMes($datetime)
+{
+    $fecha = Date::create($datetime->year, $datetime->month, $datetime->day, $datetime->hour, $datetime->minute, $datetime->second);
+    $fecha = $fecha->format('M');
+    return $fecha;
+}
