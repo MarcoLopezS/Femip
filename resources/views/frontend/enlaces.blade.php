ead3f80a9f3f89c17dee78e36ddc2f5dbc372d81
Marco Lopez <mlopez@marostdevelopers.com>
2016-09-06 02:41:30 -0500

    logos de enlaces

---
+++ resources/views/frontend/enlaces.blade.php
@@ -0,0 +1,250 @@
+@extends('layouts.frontend')
+
+@section('contenido_header')
+@endsection
+
+@section('contenido_body')
+
+    <section class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Enlaces</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="">
+                <ul class="portfolio-list enlaces" data-sort-id="portfolio">
+
+                    <li class="col-md-1-5">
+                        <div class="portfolio-item">
+                            <a href="http://clubdelabancaycomercio.com/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/club-banca-comercio.jpg" class="img-responsive" alt="Club de la Banca y Comercio">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-1-5">
+                        <div class="portfolio-item">
+                            <a href="http://www.llicaperuanitos.org/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/llica-peruanitos.jpg" class="img-responsive" alt="Llica Peruanitos">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-1-5">
+                        <div class="portfolio-item">
+                            <a href="http://visionsofperu.com/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/visions-of-peru.jpg" class="img-responsive" alt="Visions of Peru">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-1-5">
+                        <div class="portfolio-item">
+                            <a href="http://www.thinkfirst-peru.org/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/think-first.jpg" class="img-responsive" alt="Think First">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-1-5">
+                        <div class="portfolio-item">
+                            <a href="http://www.npaf-peru.org/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/npaf.jpg" class="img-responsive" alt="Neurosurgical Peruvian American Foundation (NPAF)">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <div class="col-md-12">
+                        <div class="heading heading-border heading-middle-border">
+                            <h2>Enlaces con el Estado</h2>
+                        </div>
+                    </div>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.sunat.gob.pe/orientacionaduanera/incentivosmigratorios/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/sunat.png" class="img-responsive" alt="Superintendencia Nacional de Aduanas y de Administración Tributaria - SUNAT">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.peru.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/portal-estado-peruano.png" class="img-responsive" alt="Portal del Estado Peruano">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.rree.gob.pe/SitePages/home.aspx" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/mre.png" class="img-responsive" alt="Ministerio de Relaciones Exteriores">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.mimp.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/mimp.png" class="img-responsive" alt="Ministerio de la Mujer y Poblaciones Vulnerables">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.promperu.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/promperu.jpg" class="img-responsive" alt="PromPerú">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.congreso.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/congreso-republica.png" class="img-responsive" alt="Congreso de la República">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="https://www.reniec.gob.pe/portal/intro.htm" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/reniec.png" class="img-responsive" alt="Reniec">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="http://www.jne.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/jne.png" class="img-responsive" alt="Jurado Nacional de Elecciones - JNE">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="col-md-3">
+                        <div class="portfolio-item">
+                            <a href="https://www.onpe.gob.pe/" target="_blank">
+                                <span class="thumb-info thumb-info-lighten thumb-info-no-zoom">
+                                    <span class="thumb-info-wrapper">
+                                        <img src="/imagenes/logos/onpe.png" class="img-responsive" alt="Oficina Nacional de Procesos Electorales - ONPE">
+                                        <span class="thumb-info-action">
+                                            <span class="thumb-info-action-icon"><i class="fa fa-link"></i></span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
+@section('contenido_footer')
+@endsection
