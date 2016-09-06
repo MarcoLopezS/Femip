f02809c34c29578acc1a2679df56580169900835
Marco Lopez <mlopez@marostdevelopers.com>
2016-09-06 04:08:34 -0500

    agregando script de google analytics

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -151,6 +151,17 @@
             <!-- Theme Initialization Files -->
     {!! HTML::script('js/theme.init.js') !!}
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-20229980-15', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
     @yield('contenido_footer')
 
 </body>
