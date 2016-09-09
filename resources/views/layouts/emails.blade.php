f9c90c986f6900e72d52dc78e7c920a1dff93422
Marco Lopez <mlopez@marostdevelopers.com>
2016-09-09 01:38:33 -0500

    view email de mensaje de form contacto

---
+++ resources/views/layouts/emails.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Femip</title>
+    <link href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet" type="text/css">
+
+    @yield('contenido_header')
+
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="margin: 0;padding: 0;min-width: 100%;background-color: #f2f2f2;">
+    <div class="email_body" style="padding: 32px 10px;text-align: center;background-color: #f2f2f2;">
+        <!--[if (gte mso 9)|(IE)]>
+        <table width="580" border="0" cellspacing="0" cellpadding="0" align="center" style="vertical-align: top;">
+            <tbody>
+            <tr>
+                <td width="580" align="center" valign="top">
+        <![endif]-->
+        <div class="email_container" style="display: inline-block;width: 100%;vertical-align: top;text-align: center;margin: 0 auto;max-width: 580px;font-size: 0 !important;">
+            <table class="header" width="100%" border="0" cellspacing="0" cellpadding="0" style="border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                <tbody>
+                <tr>
+                    <td class="header_cell col-bottom-0" align="center" valign="top" style="padding: 0;text-align: center;padding-bottom: 16px;-webkit-border-radius: 4px 4px 0 0;border-radius: 4px 4px 0 0;background-color: #ffffff;font-size: 0 !important;">
+                        <!--[if (gte mso 9)|(IE)]>
+                        <table width="580" border="0" cellspacing="0" cellpadding="0" align="center" style="vertical-align: top;">
+                            <tbody>
+                            <tr>
+                                <td width="580" align="center" valign="top">
+                        <![endif]-->
+                        <div class="row" style="display: inline-block;width: 100%;vertical-align: top;text-align: center;max-width: 580px;margin: 0 auto;">
+                            <!--[if (gte mso 9)|(IE)]>
+                            <table width="580" border="0" cellspacing="0" cellpadding="0" align="center" style="vertical-align: top;">
+                                <tbody>
+                                <tr>
+                                    <td width="580" align="center" valign="top">
+                            <![endif]-->
+                            <div class="col-3" style="display: inline-block;width: 100%;vertical-align: top;text-align: center;max-width: 580px;">
+                                <table class="column" width="100%" border="0" cellspacing="0" cellpadding="0" style="border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;vertical-align: top;">
+                                    <tbody>
+                                    <tr>
+                                        <td class="column_cell font_default" align="center" valign="top" style="padding: 16px;font-family: Helvetica, Arial, sans-serif;font-size: 20px;text-align: center;vertical-align: top;color: #f06161;font-weight: bold;padding-bottom: 0;padding-top: 16px;">
+                                            <a href="#" style="display: inline-block;text-decoration: none;font-family: Helvetica, Arial, sans-serif;color: #ffffff;">
+                                                <img src="{{ url('imagenes/logo.png') }}" width="250" height="100" alt="Consensus" style="line-height: 1;outline: none;border: 0;text-decoration: none;-ms-interpolation-mode: bicubic;mso-line-height-rule: exactly;max-width: 250px;height: auto;">
+                                            </a>
+                                        </td><!-- /.column_cell -->
+                                    </tr>
+                                    </tbody>
+                                </table><!-- /.column -->
+                            </div><!-- /.col-3 -->
+                            <!--[if (gte mso 9)|(IE)]>
+                            </td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <![endif]-->
+                        </div><!-- /.row -->
+                        <!--[if (gte mso 9)|(IE)]>
+                        </td>
+                        </tr>
+                        </tbody>
+                        </table>
+                        <![endif]-->
+                    </td><!-- /.header_cell -->
+                </tr>
+                </tbody>
+            </table><!-- /.header -->
+
+            @yield('contenido_body')
+
+            <table class="footer" width="100%" border="0" cellspacing="0" cellpadding="0" style="border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                <tbody>
+                <tr>
+                    <td class="footer_cell" align="center" valign="top" style="padding: 0;text-align: center;padding-bottom: 16px;-webkit-border-radius: 0 0 4px 4px;border-radius: 0 0 4px 4px;background-color: #ffffff;border-top: 1px solid;border-color: #f2f2f2;font-size: 0 !important;">
+                        <!--[if (gte mso 9)|(IE)]>
+                        <table width="580" border="0" cellspacing="0" cellpadding="0" align="center" style="vertical-align: top;">
+                            <tbody>
+                            <tr>
+                                <td width="580" align="center" valign="top">
+                        <![endif]-->
+                        <div class="row" style="display: inline-block;width: 100%;vertical-align: top;text-align: center;max-width: 580px;margin: 0 auto;">
+                            <!--[if (gte mso 9)|(IE)]>
+                            <table width="580" border="0" cellspacing="0" cellpadding="0" align="center" style="vertical-align: top;">
+                                <tbody>
+                                <tr>
+                                    <td width="390" align="center" valign="top">
+                            <![endif]-->
+                            <div class="col-13 col-bottom-0" style="display: inline-block;width: 100%;vertical-align: top;text-align: center;max-width: 100%;">
+                                <table class="column" width="100%" border="0" cellspacing="0" cellpadding="0" style="border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;vertical-align: top;">
+                                    <tbody>
+                                    <tr>
+                                        <td class="column_cell font_default" align="center" valign="top" style="padding: 16px;font-family: Helvetica, Arial, sans-serif;font-size: 15px;text-align: left;vertical-align: top;color: #b3b3b5;padding-bottom: 0;padding-top: 16px;">
+                                            <strong>Femip</strong><br>
+                                        </td><!-- /.column_cell -->
+                                    </tr>
+                                    </tbody>
+                                </table><!-- /.column -->
+                            </div><!-- /.col-13 -->
+                            <!--[if (gte mso 9)|(IE)]>
+                            </td>
+                            <td width="190" align="center" valign="top">
+                            <![endif]-->
+                            <!--[if (gte mso 9)|(IE)]>
+                            </td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <![endif]-->
+                        </div><!-- /.row -->
+                        <!--[if (gte mso 9)|(IE)]>
+                        </td>
+                        </tr>
+                        </tbody>
+                        </table>
+                        <![endif]-->
+                    </td><!-- /.footer_cell -->
+                </tr>
+                </tbody>
+            </table><!-- /.footer -->
+        </div><!-- /.email_container -->
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </tbody>
+        </table>
+        <![endif]-->
+    </div><!-- /.email_body -->
+</body>
+</html>
